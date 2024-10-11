fb2fee9978c015647a8fa9675c413b99eb9f5e62
tzwinger <zwingerthomas@gmail.com>
2024-10-11 22:58:35 +0300

    added build recipies for Ubuntu24 cloud on CPouta

---
+++ Cloudinstance/InstallElmerDependencies.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+mkdir ElmerDependencies
+cd ElmerDependencies
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "get nn and csa sources"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+git clone https://github.com/sakov/nn-c 
+git clone https://github.com/sakov/csa-c
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "compile nn and csa"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+cd /home/ubuntu/ElmerDependencies/nn-c/nn
+CFLAGS="-fPIC -O2" ./configure 
+# manually compile trinagle.c with -fPIC (for some odd reason CFLAGS are ignored here)
+mv makefile makefile.tmp
+awk '/^CFLAGS_TRIANGLE/{gsub(/=/,"= -fPIC ")};{print}' makefile.tmp > makefile
+rm -f makefile.tmp
+make -j 2
+sudo make install
+cd /home/ubuntu/ElmerDependencies/csa-c/csa
+CFLAGS="-fPIC -O2" ./configure 
+make -j 2
+sudo make install
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "get mmg sources"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+#wget https://github.com/MmgTools/mmg/archive/v5.3.10.tar.gz
+#tar xvzf v5.3.10.tar.gz
+cd /home/ubuntu/ElmerDependencies
+git clone  https://github.com/MmgTools/mmg.git
+cd mmg
+git checkout develop
+#git reset --hard 565d6849
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "compile mmg"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+mkdir build
+cd build
+cmake -D CMAKE_INSTALL_PREFIX="/usr/local" -D CMAKE_BUILD_TYPE=RelWithDebInfo -D BUILD_SHARED_LIBS:BOOL=TRUE -D MMG_INSTALL_PRIVATE_HEADERS=ON -D CMAKE_C_FLAGS="-fPIC  -g" -D CMAKE_CXX_FLAGS="-fPIC -std=c++11 -g"  ..
+make -j $(nproc) install
+#cd build/
+#cmake -S "/home/ubuntu/ElmerDependencies/mmg" -DBUILD_SHARED_LIBS:BOOL=TRUE -DLIBMMG3D_SHARED:BOOL=TRUE -DLIBMMG2D_SHARED:BOOL=TRUE -D CMAKE_C_FLAGS="-fPIC -O2" -D CMAKE_CXX_FLAGS="-fPIC -O2"
+make
+sudo make install
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "get ParMMG sources"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+cd /home/ubuntu/ElmerDependencies
+git clone https://github.com/MmgTools/ParMmg.git
+cd ParMmg
+git checkout develop
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "compile ParMMG"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+mkdir build
+cd build
+cmake -D CMAKE_INSTALL_PREFIX="/usr/local" -D CMAKE_BUILD_TYPE=RelWithDebInfo -D BUILD_SHARED_LIBS:BOOL=TRUE -D DOWNLOAD_MMG=OFF  -D MMG_DIR="/usr/local"  ..
+make -j $(nproc) install
+# cleaning up
+cd /home/ubuntu/
+echo "if you want to clean up, then remove:"
+echo "rm -fr /home/ubuntu/ElmerDependencies"
+echo "#############################"
+echo "#############################"
+echo "#############################"
+echo "ALL DONE"
+echo "#############################"
---
+++ Cloudinstance/buildelmer.sh
@@ -0,0 +1,59 @@
+
+
+  
+#!/bin/bash
+echo "cloning git repository (be patient, this make take a while)"
+git clone https://github.com/ElmerCSC/elmerfem.git
+cd elmerfem
+echo "Adding Zoltan as git-submodule:"
+git submodule sync 
+git submodule update --init 
+echo "starting to configure"
+cd ..
+rm -fr build/*
+mkdir build
+cd build
+if  ! cmake -S /home/ubuntu/elmerfem -DCMAKE_INSTALL_PREFIX="/usr/local"\
+      -DWITH_OpenMP:BOOLEAN=TRUE \
+      -DWITH_MPI:BOOLEAN=TRUE \
+      -DWITH_LUA:BOOL=TRUE \
+      -DWITH_Mumps:BOOL=TRUE \
+      -DWITH_Hypre:BOOL=TRUE \
+      -DHypre_INCLUDE_DIR="/usr/include/hypre" \
+      -DWITH_GridDataReader:BOOL=TRUE \
+      -DWITH_Zoltan:BOOL=TRUE \
+      -DWITH_ElmerIce:BOOL=TRUE \
+      -DWITH_ScatteredDataInterpolator=BOOL=TRUE \
+      -DCSA_LIBRARY="/usr/local/lib/libcsa.a" \
+      -DCSA_INCLUDE_DIR="/usr/local/include" \
+      -DNN_LIBRARY="/usr/local/lib/libnn.a" \
+      -DNN_INCLUDE_DIR="/usr/local/include" \
+      -DWITH_MMG:BOOL=TRUE \
+      -DPARMMGROOT="/usr/local" \
+      -DMMG_INCLUDE_DIR="/usr/local/include" \
+      -DMMG_LIBRARY="/usr/local/lib/libmmg.so" \
+      -DWITH_NETCDF:BOOL=TRUE \
+      -DNETCDF_LIBRARY="/usr/lib/x86_64-linux-gnu/libnetcdf.so" \
+      -DNETCDFF_LIBRARY="/usr/lib/x86_64-linux-gnu/libnetcdff.so" \
+      -DNETCDF_INCLUDE_DIR="/usr/include" \
+      -DWITH_ELMERGUI:BOOL=FALSE \
+      -DWITH_QT5:BOOL=TRUE \
+      -DQWT_INCLUDE_DIR="/usr/include/qwt" \
+      -DQWT_LIBRARY="/usr/lib/libqwt-qt5.so" \
+      -DWITH_OCC:BOOL=TRUE \
+      -DWITH_PARAVIEW:BOOL=TRUE \
+      -DWITH_QWT:BOOL=TRUE \
+      -DWITH_VTK:BOOL=FALSE \
+      -DWITH_MATC:BOOL=FALSE \
+      -DWITH_PYTHONQT:BOOL=FALSE \
+      -DMPIEXEC_MAX_NUMPROCS="2" \
+      -DMPIEXEC_NUMPROC_FLAG="--oversubscribe -np" \
+      -Wno-dev; then
+    echo "Configuration failed"
+    cd ..
+    exit -1
+fi
+echo "Starting compilation - this may take a while (depending on the speed of your computer's CPU"
+make -j 4 install
+echo "done with compilation"
+exit 0
